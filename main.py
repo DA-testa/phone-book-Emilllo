95dcdbed2cdef1b201c3fa8de5776f361c021544
Emilllo <Emily.Ostashevsky@gmail.com>
2023-04-02 20:56:24 +0300

    Update

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-# python3
+# 221RDB231 Emīlija Ostaševska 4.gr
+
 
 class Query:
     def __init__(self, query):
@@ -7,41 +8,32 @@ class Query:
         if self.type == 'add':
             self.name = query[2]
 
+
 def read_queries():
     n = int(input())
     return [Query(input().split()) for i in range(n)]
 
+
 def write_responses(result):
     print('\n'.join(result))
 
+
 def process_queries(queries):
+    contacts = {}
     result = []
-    # Keep list of all existing (i.e. not deleted yet) contacts.
-    contacts = []
-    for cur_query in queries:
-        if cur_query.type == 'add':
-            # if we already have contact with such number,
-            # we should rewrite contact's name
-            for contact in contacts:
-                if contact.number == cur_query.number:
-                    contact.name = cur_query.name
-                    break
-            else: # otherwise, just add it
-                contacts.append(cur_query)
-        elif cur_query.type == 'del':
-            for j in range(len(contacts)):
-                if contacts[j].number == cur_query.number:
-                    contacts.pop(j)
-                    break
+
+    for current_query in queries:
+        if current_query.type == 'add':
+            contacts[current_query.number] = current_query.name
+        elif current_query.type == 'del' and current_query.number in contacts:
+            del contacts[current_query.number]
         else:
             response = 'not found'
-            for contact in contacts:
-                if contact.number == cur_query.number:
-                    response = contact.name
-                    break
+            if current_query.number in contacts:
+                response = contacts[current_query.number]
             result.append(response)
     return result
 
+
 if __name__ == '__main__':
     write_responses(process_queries(read_queries()))
-
